8436b00fd85dbe5c72eeb4b5b39adcc5eba8e250
mhgzadeh <m.hgzadeh@gmail.com>
2025-11-03 08:55:51 +0100

    v.0.2.0; add AuthService, integrate authentication in API calls

--- final/src/index.js
+++ final/src/index.js
@@ -3,64 +3,38 @@ import "bootstrap/dist/js/bootstrap";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./css/main.css";
 import "cesium/Build/Cesium/Widgets/widgets.css";
-import { Cesium3DTileset, createWorldTerrainAsync, Ion, Viewer } from "cesium";
-import { accessToken, assetIds } from "./js/CesiumConfig";
-import { fetchAndDisplayPipes } from "./js/FetchAndDisplayPipes";
-import { fetchApplyBuildingColors } from "./js/FetchApplyBuildingColors";
+import {Cesium3DTileset, createWorldTerrainAsync, Ion, Viewer} from "cesium";
+import {accessToken, assetIds} from "./js/CesiumConfig";
+import {fetchApplyBuildingColors} from "./js/FetchApplyBuildingColors";
+import {getAuthToken} from "./js/AuthService";
 
 Ion.defaultAccessToken = accessToken;
 
+let authToken = null;
+try {
+    authToken = await getAuthToken();
+} catch (error) {
+    console.error("Failed to authenticate:", error);
+    alert("Authentication failed. Please check your credentials.");
+}
+
 const viewer = new Viewer("cesiumContainer", {
-  terrainProvider: await createWorldTerrainAsync(),
+    terrainProvider: await createWorldTerrainAsync(),
 });
 
 const tileSet = {
-  cityGml: await Cesium3DTileset.fromIonAssetId(assetIds.cityGml),
+    cityGml: await Cesium3DTileset.fromIonAssetId(assetIds.cityGml),
 };
 
 viewer.scene.primitives.add(tileSet.cityGml);
 
 await viewer.zoomTo(tileSet.cityGml);
 
-const apiMainPipe =
-  "https://backend.gisworld-tech.com/geospatial/main-pipes/?project_id=00001";
-const apiHousePipe =
-  "https://backend.gisworld-tech.com/geospatial/house-pipes/?project_id=00001";
 const apiBuildingUrl =
-  "https://backend.gisworld-tech.com/geospatial/buildings-energy/?project_id=00001";
+    "https://backend.gisworld-tech.com/geospatial/communities/08417008/buildings/";
 
-fetchApplyBuildingColors(apiBuildingUrl, tileSet.cityGml, viewer);
-
-fetchAndDisplayPipes({
-  viewer: viewer,
-  apiUrl: apiMainPipe,
-  pipeType: "Main",
-  circleRadius: 0.8,
-});
-
-fetchAndDisplayPipes({
-  viewer: viewer,
-  apiUrl: apiHousePipe,
-  pipeType: "House",
-  circleRadius: 0.4,
-});
+fetchApplyBuildingColors(apiBuildingUrl, tileSet.cityGml, viewer, authToken);
 
 document.getElementById("fetchData").addEventListener("click", () => {
-  fetchApplyBuildingColors(apiBuildingUrl, tileSet.cityGml, viewer);
-  fetchAndDisplayPipes({
-    viewer: viewer,
-    apiUrl: apiMainPipe,
-    pipeType: "Main Pipe",
-    circleRadius: 0.8,
-    fetchData: true,
-    removeAll: true,
-  });
-  fetchAndDisplayPipes({
-    viewer: viewer,
-    apiUrl: apiHousePipe,
-    pipeType: "House Connecting Pipe",
-    circleRadius: 0.4,
-    fetchData: true,
-    removeAll: false,
-  });
+    fetchApplyBuildingColors(apiBuildingUrl, tileSet.cityGml, viewer, authToken);
 });
