5cad3e4381240d4e295b9351f4b6e0d361025a2e
mhgzadeh <m.hgzadeh@gmail.com>
2025-11-04 13:19:39 +0100

    add some todos

--- start/src/js/CesiumConfig.js
+++ start/src/js/CesiumConfig.js
@@ -1,7 +1,7 @@
-const accessToken =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIwZDNjMzA1Mi04ZGNmLTQ5YTgtOThjZi00NzZhNWMzNj" +
-  "FkZDIiLCJpZCI6MTkxNTA0LCJpYXQiOjE3MjM0OTE3MTh9.iv_r8IxVBItODjZWmSO8WjFCxSSrmu6rQ7qnGYxZNkM";
+// TODO 1: Add your Cesium access token here
+const accessToken = "";
 
-const assetIds = { cityGml: 3123354 };
+// TODO 2: Add your asset IDs here
+const assetIds = {cityGml: ''};
 
-export { accessToken, assetIds };
+export {accessToken, assetIds};
